5e1b96b68e7f3ebf0ca47568a0bc7fb87cc4359d
dan.barcan <dan.barcan1994@gmail.com>
2018-06-13 21:39:44 +0300

    added cors mapping

--- src/main/java/ro/apaic/registry/RegistryApplication.java
+++ src/main/java/ro/apaic/registry/RegistryApplication.java
@@ -2,11 +2,32 @@ package ro.apaic.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class RegistryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
